cf73c0aee98c24948203bf5c514df86fa0a0c8db
Wubbo Bos <wubbo@addnoise.nl>
2018-10-16 11:32:11 +0200

    New static approach to form rendering

--- FormKit/src/Form.php
+++ FormKit/src/Form.php
@@ -4,19 +4,45 @@ namespace FormKit;
 
 class Form
 {
-    protected $name;
-    protected $fields = [];
-    protected $fieldsByName = [];
+    protected static $postData = [];
+    protected static $formNames = [];
+    protected static $currFormName = null;
 
     public function __construct($name)
     {
         $this->name = $name;
     }
 
-    public function addField(Field $field)
+    protected static function buildAttributes($attributes)
     {
-        $field->setForm($this);
-        $this->fields[] = $field;
-        $this->fieldsByName[$field->getName()] = $field;
+        $html = '';
+        foreach ($attributes as $key => $value) {
+            $html .= ' '.$key.'="'.htmlspecialchars($value).'"';
+        }
+        return $html;
+    }
+
+    protected static function buildId($name)
+    {
+        $id = trim(preg_replace("/[^a-zA-Z0-9]+/", "-", $name), "-");
+        if (self::$currFormName) {
+            $id = self::$currFormName . "-" . $id;
+        }
+        return $id;
+    }
+
+    public static function open(array $options)
+    {
+        self::$formNames[] = self::$currFormName = $options['name']??[];
+        $tag = '<form method="' . ($options['method']??'post') . '" action="' . $options['action'] . '" enctype="' . ($options['enctype']??'application/x-www-form-urlencoded') . '"';
+        unset($options['method'], $options['action'], $options['enctype']);
+        $tag .= self::buildAttributes($options) . '>';
+        return $tag;
+    }
+
+    public static function textField($name, $initValue = '', $attributes = [])
+    {
+        $value = isset(self::$postData[$name]) ? self::$postData[$name] : $initValue;
+        return '<input type="text" name="' . $name . '" id="' . self::buildId($name) . '" value="' . $value . '"' . self::buildAttributes($attributes) . ' />';
     }
 }
